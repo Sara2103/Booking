4c290a55ec85cfac3c77b593078decc2d59fdf70
mitrosa <sara.mitrovic@safenet.com>
2021-12-06 18:26:02 +0100

    added BasePage and BaseTest and edited other classes

--- src/test/java/pages/BookingHomePage.java
+++ src/test/java/pages/BookingHomePage.java
@@ -1,11 +1,18 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BookingHomePage {
+public class BookingHomePage extends BasePage {
 
     WebDriver driver;
     WebDriverWait wait;
 
+    public BookingHomePage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/test/java/steps/CucumberSteps.java
+++ src/test/java/steps/CucumberSteps.java
@@ -1,11 +1,25 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import tests.BaseTest;
 
 
-public class CucumberSteps{
+public class CucumberSteps extends BaseTest {
+
+    @Before
+    public void init(){
+        init("Chrome", "96", 30);
+    }
 
     @Given("I am on booking home page")
     public void iAmOnBookingHomePage() {
+        driver.get("https://www.booking.com/");
+    }
+
+    @After
+    public void quit() {
+        quitDriver();
     }
 }

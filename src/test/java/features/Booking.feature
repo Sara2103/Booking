3972ff2af27cf718f4dcd6ec0cc1ec93a776a852
mitrosa <sara.mitrovic@safenet.com>
2021-12-06 17:58:50 +0100

    added folders in test/java

---
+++ src/test/java/features/Booking.feature
@@ -0,0 +1,7 @@
+Feature: Test
+
+  Scenario: Test
+    Given I am on booking home page
+
+
+
---
+++ src/test/java/steps/CucumberSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+
+public class CucumberSteps{
+
+    @Given("I am on booking home page")
+    public void iAmOnBookingHomePage() {
+    }
+}
